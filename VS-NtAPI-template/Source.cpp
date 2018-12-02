6ae99bb39ec6b45258eda24ed37758c8cc686024
Yonatan Erez <yonatanerez0@gmail.com>
2018-12-02 02:09:50 +0200

    init commit: Native  api project

---
+++ VS-NtAPI-template/Source.cpp
@@ -0,0 +1,21 @@
+/************************************************************************/
+/* Visual Studio Native Project | Copyright (c) Yonatan Erez(JohnE) 2018*/
+/* _________________________Last Added Functions________________________*/
+/*   Date         Who      Function                                     */
+/* --------  ------------  --------                                     */
+/* 01.12.18  Yonatan Erez  RtlExitUserProcess                           */
+/* 01.12.18  Yonatan Erez  RtlExitUserThread                            */
+/* 01.12.18  Yonatan Erez  LdrShutdownProcess                           */
+/* 01.12.18  Yonatan Erez  LdrShutdownThread                            */
+/* 01.12.18  Yonatan Erez  RtlCreateProcessParametersEx                 */
+/************************************************************************/
+
+#include "ntdll.h"
+#pragma comment(lib,"ntdll.lib")
+
+INT wmain(INT argc, WCHAR *argv[], WCHAR *envp[])
+{	
+	// Your Code
+
+	return (INT)STATUS_SUCCESS;
+}
